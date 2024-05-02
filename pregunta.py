4b53469e1ee6e2c379d414b671aa5756ec31d1f6
Kbarrientosr <160262434+Kbarrientosr@users.noreply.github.com>
2024-05-01 20:27:29 -0500

    Add files via upload

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,32 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    filename = "clusters_report.txt"
+    with open (filename, "r") as file:
+        lines_df = file.readlines()
 
+    datos = []
+    
+    for line in lines_df[4:]:
+        word = line.strip()
+        word = word.split()
+        
+        if len(word)>0 and word[0].isdigit():
+            cluster = int(word[0])
+            cantidad_de_palabras_clave = int(word[1])
+            porcentaje_de_palabras_clave = float(word[2].replace(",","."))
+            principales_palabras_clave = " ".join(word[3:]).replace(",", ", ").replace("%","").replace("  ", " ").replace("   "," ").replace("    "," ").replace(".","")
+            principales_palabras_clave = principales_palabras_clave.strip()   
+            datos.append({"cluster": cluster, "cantidad_de_palabras_clave": cantidad_de_palabras_clave, "porcentaje_de_palabras_clave": porcentaje_de_palabras_clave, "principales_palabras_clave": principales_palabras_clave})
+    
+        elif len(word)>0:
+            principales_palabras_clave = " ".join(word[0:]).replace(",", ", ").replace("%","").replace("  ", " ").replace("   "," ").replace("    "," ").replace(".","")
+            principales_palabras_clave = principales_palabras_clave.strip()
+            datos.append({"cluster": cluster, "cantidad_de_palabras_clave": cantidad_de_palabras_clave, "porcentaje_de_palabras_clave": porcentaje_de_palabras_clave, "principales_palabras_clave": principales_palabras_clave})
+
+    df = pd.DataFrame(datos)
+    df = df.groupby(["cluster", "cantidad_de_palabras_clave","porcentaje_de_palabras_clave"])["principales_palabras_clave"].agg(lambda x: " ".join(x.astype(str))).reset_index()
+        
     return df
